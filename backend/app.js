c3046588e87e35106d95c75a5dd6631867783ec9
Usman <140506695+usmanbhat@users.noreply.github.com>
2025-01-14 12:11:12 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -72,7 +72,7 @@ app.use(feedbackRoutes)
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/frontend/index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist/frontend/index.html'));
 });
 
  //app.get("/", async (req, res) => {
